225e2709213cf19af33c9844e0f90f0f68793f7d
Wolfsquared <a.o.swanepoel@proton.me>
2024-02-17 09:42:09 +0200

    intial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreeCodeCamp_ASPDotNet_Crash_Course.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
